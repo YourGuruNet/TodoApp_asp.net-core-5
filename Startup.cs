081e8ec8c0b930b322a8cbc689888f393bf07a0b
arvis <info@yourgurunet.com>
2021-03-09 13:05:14 +0200

    Register the database context

--- Startup.cs
+++ Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TodoApp.Models;
 
 namespace TodoApp
 {
@@ -27,6 +29,8 @@ namespace TodoApp
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<TodoContext>(opt =>
+                                               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
